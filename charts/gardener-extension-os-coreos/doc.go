bd2696fed4402ba3d8d01e483f3c6ebc26be54c6
Luca Bernstein <luca.bernstein@sap.com>
2024-06-05 16:33:43 +0200

    Adjust paths after vendor removal
    [make tidy]

--- charts/gardener-extension-os-coreos/doc.go
+++ charts/gardener-extension-os-coreos/doc.go
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-//go:generate sh -c "../../vendor/github.com/gardener/gardener/hack/generate-controller-registration.sh os-coreos . $(cat ../../VERSION) ../../example/controller-registration.yaml OperatingSystemConfig:coreos OperatingSystemConfig:flatcar"
+//go:generate sh -c "bash $GARDENER_HACK_DIR/generate-controller-registration.sh os-coreos . $(cat ../../VERSION) ../../example/controller-registration.yaml OperatingSystemConfig:coreos OperatingSystemConfig:flatcar"
 
 // Package chart enables go:generate support for generating the correct controller registration.
 package chart
