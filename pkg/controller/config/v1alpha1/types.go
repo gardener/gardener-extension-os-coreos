b2451de3b39cec33a58172244b84ddad4877a29b
Niclas Schad <niclas.schad@stackit.cloud>
2024-11-12 15:14:49 +0100

    improve description in config API
    Signed-off-by: Niclas Schad <niclas.schad@stackit.cloud>

--- pkg/controller/config/v1alpha1/types.go
+++ pkg/controller/config/v1alpha1/types.go
@@ -11,11 +11,11 @@ type ExtensionConfig struct {
 	metav1.TypeMeta `json:",inline"`
 	// UseNTP whether to toggle NTP or not
 	UseNTP *bool `json:"useNTP"`
-	// NTPConfig ntpConfig
+	// NTPConfig config struct that is directly templated into ntp-config.conf.tpl
 	NTPConfig *NTPConfig `json:"ntpConfig"`
 }
 
-// NTPConfig is the struct used in the ntp-config template file
+// NTPConfig is the struct used in the ntp-config.conf.tpl template file
 type NTPConfig struct {
 	// NTPServers List of ntp servers
 	NTPServers []string `json:"ntpServers"`
