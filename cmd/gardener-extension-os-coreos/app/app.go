203ed1bf4521fe031d08cdeaef301c016fdf08bc
Thomas Buchner <thomas.buchner@sap.com>
2025-01-30 10:08:09 +0100

    adapt to upstream changes

--- cmd/gardener-extension-os-coreos/app/app.go
+++ cmd/gardener-extension-os-coreos/app/app.go
@@ -13,7 +13,7 @@ import (
 	"github.com/gardener/gardener/extensions/pkg/util"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	"github.com/spf13/cobra"
-	componentbaseconfig "k8s.io/component-base/config"
+	componentbaseconfig "k8s.io/component-base/config/v1alpha1"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
