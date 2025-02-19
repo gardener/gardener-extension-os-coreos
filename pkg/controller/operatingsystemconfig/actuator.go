7271eeb7719444ff6fc3b3d3d0390d490af6fe0a
Oliver Götz <47362717+oliver-goetz@users.noreply.github.com>
2025-02-19 10:01:48 +0100

    Ensure that the provision OSC script runs only once (#166)

--- pkg/controller/operatingsystemconfig/actuator.go
+++ pkg/controller/operatingsystemconfig/actuator.go
@@ -126,6 +126,9 @@ systemctl enable docker && systemctl restart docker
 `, unit.Name, unit.Name)
 	}
 
+	// The provisioning script must run only once.
+	script = operatingsystemconfig.WrapProvisionOSCIntoOneshotScript(script)
+
 	return script, nil
 }
 
