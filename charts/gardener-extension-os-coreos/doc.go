5ca1c7e73f420283116c02832d086bb83f248cb1
Robin Schneider <robin.schneider@stackit.cloud>
2025-12-16 17:47:10 +0100

    Add types for all flatcar release channel

--- charts/gardener-extension-os-coreos/doc.go
+++ charts/gardener-extension-os-coreos/doc.go
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-//go:generate sh -c "bash $GARDENER_HACK_DIR/generate-controller-registration.sh os-coreos . $(cat ../../VERSION) ../../example/controller-registration.yaml OperatingSystemConfig:coreos OperatingSystemConfig:flatcar"
+//go:generate sh -c "bash $GARDENER_HACK_DIR/generate-controller-registration.sh os-coreos . $(cat ../../VERSION) ../../example/controller-registration.yaml OperatingSystemConfig:coreos OperatingSystemConfig:flatcar OperatingSystemConfig:flatcar-alpha OperatingSystemConfig:flatcar-beta OperatingSystemConfig:flatcar-stable OperatingSystemConfig:flatcar-lts"
 
 // Package chart enables go:generate support for generating the correct controller registration.
 package chart
